e24573191a258920cffd4fae74f8f70febadd3d5
Lepo00 <luca.leporati@outlook.com>
2021-01-22 18:45:43 +0100

    Prova

--- Backend/ManagementSpring/src/main/java/it/jac/ManagementSpring/ManagementSpringApplication.java
+++ Backend/ManagementSpring/src/main/java/it/jac/ManagementSpring/ManagementSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ManagementSpringApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello world");
 		SpringApplication.run(ManagementSpringApplication.class, args);
 	}
 
