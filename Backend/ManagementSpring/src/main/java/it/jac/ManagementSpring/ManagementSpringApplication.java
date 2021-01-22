59dc81cf618a51af16e0350cc3616c7030b7e40a
Lepo00 <luca.leporati@outlook.com>
2021-01-22 18:35:43 +0100

    Spring initializr

---
+++ Backend/ManagementSpring/src/main/java/it/jac/ManagementSpring/ManagementSpringApplication.java
@@ -0,0 +1,13 @@
+package it.jac.ManagementSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementSpringApplication.class, args);
+	}
+
+}
