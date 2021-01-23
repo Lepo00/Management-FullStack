bb8bf9a884ffd8f96b343982dbf82130e7290afe
Lepo00 <luca.leporati@outlook.com>
2021-01-23 12:38:13 +0100

    fix file sql

---
+++ Backend/ManagementSpring/src/main/resources/unit_of_measure_insert_table.sql
@@ -0,0 +1,8 @@
+INSERT INTO `unit_of_measure` (`id`, `unit`, `name`) VALUES
+(1, 'g', 'grammi'),
+(2, 'kg', 'chilogrammi'),
+(3, 'ml', 'millilitri'),
+(4, 'l', 'litri'),
+(5, 'm', 'metri'),
+(6, 'km', 'chilometri');
+
