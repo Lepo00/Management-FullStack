89d0abb4d20d0d1fb0348f860352eab9cedc7667
Lepo00 <luca.leporati@outlook.com>
2021-01-23 12:39:08 +0100

    Foreign key ok

--- Backend/ManagementSpring/src/main/java/it/jac/ManagementSpring/model/Invoice.java
+++ Backend/ManagementSpring/src/main/java/it/jac/ManagementSpring/model/Invoice.java
@@ -1,9 +1,13 @@
 package it.jac.ManagementSpring.model;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -46,5 +50,82 @@ public class Invoice extends AuditModel{
 	
 	@Column(name = "payment_type")
 	private String paymentType;
+	
+	@ManyToMany
+	@JoinTable(name="invoice_item", joinColumns = @JoinColumn(name="item_id"),
+	inverseJoinColumns=@JoinColumn(name="invoice_id"))
+	private List<Item> items;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getIntestatario() {
+		return intestatario;
+	}
+
+	public void setIntestatario(String intestatario) {
+		this.intestatario = intestatario;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Date getShipmentDate() {
+		return shipmentDate;
+	}
+
+	public void setShipmentDate(Date shipmentDate) {
+		this.shipmentDate = shipmentDate;
+	}
+
+	public int getNetPrice() {
+		return netPrice;
+	}
+
+	public void setNetPrice(int netPrice) {
+		this.netPrice = netPrice;
+	}
+
+	public int getTaxes() {
+		return taxes;
+	}
+
+	public void setTaxes(int taxes) {
+		this.taxes = taxes;
+	}
+
+	public int getTotPrice() {
+		return totPrice;
+	}
+
+	public void setTotPrice(int totPrice) {
+		this.totPrice = totPrice;
+	}
+
+	public String getPaymentType() {
+		return paymentType;
+	}
+
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
 
 }
