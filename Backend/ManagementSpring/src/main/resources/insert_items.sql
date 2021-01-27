562894ebb730975dbfd64e4d6a4977da35e275c5
Lepo00 <luca.leporati@outlook.com>
2021-01-27 12:50:50 +0100

    insert_items sql file

---
+++ Backend/ManagementSpring/src/main/resources/insert_items.sql
@@ -0,0 +1,16 @@
+INSERT INTO `item` (`id`, `created_at`, `updated_at`, `code`, `description`, `price`, `quantity`, `unit_of_measure_id`) VALUES 
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'AAA', 'legno', '10', NULL, 2),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'BBB', 'roccia', '20', NULL, 2),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'CCC', 'acqua', '30', NULL, 4),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'DDD', 'pietra', '40', NULL, 2),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'EEE', 'fango', '50', NULL, 2),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'FFF', 'sabbia', '60', NULL, 2),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'GGG', 'veleno', '70', NULL, 3),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'HHH', 'rete', '80', NULL, 5),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'III', 'bronzo', '90', NULL, 1),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'JJJ', 'oro', '100', NULL, 1),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'KKK', 'spago', '60', NULL, 5),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'LLL', 'cavo', '70', NULL, 6),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'MMM', 'granito', '80', NULL, 5),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'NNN', 'marmo', '90', NULL, 1),
+(NULL, CURRENT_TIME(), CURRENT_TIME(), 'OOO', 'diluente', '100', NULL, 4)
