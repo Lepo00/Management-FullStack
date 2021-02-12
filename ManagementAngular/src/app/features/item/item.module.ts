6af8550d9d3e2c5ae08da823cedbc079c1ff4eda
Lepo00 <luca.leporati@outlook.com>
2021-02-12 10:50:35 +0100

    fix httpService & item

--- ManagementAngular/src/app/features/item/item.module.ts
+++ ManagementAngular/src/app/features/item/item.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ItemRoutingModule } from './item-routing.module';
 import { ItemComponent } from './item.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CoreModule } from 'src/app/core/core.module';
 
 
 @NgModule({
   declarations: [ItemComponent],
   imports: [
-    CommonModule,
-    ItemRoutingModule
+    ItemRoutingModule,
+    SharedModule,
+    CoreModule
   ]
 })
 export class ItemModule { }
