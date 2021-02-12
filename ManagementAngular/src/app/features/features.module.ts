9641b2f069ee7d6510c15cecfedec0e46ff091ec
Lepo00 <luca.leporati@outlook.com>
2021-02-12 15:58:54 +0100

    fix width table

--- ManagementAngular/src/app/features/features.module.ts
+++ ManagementAngular/src/app/features/features.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { CoreModule } from '../core/core.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    SharedModule,
+    CoreModule
   ]
 })
 export class FeaturesModule { }
