3f5fe6b35825a98f543a87fb0f7d074f36baa659
AGhilardi <57990391+AGhilardi@users.noreply.github.com>
2021-02-23 17:43:16 +0100

    divertiti luca

--- ManagementAngular/src/app/features/invoices/invoices.component.ts
+++ ManagementAngular/src/app/features/invoices/invoices.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { InvoiceBody } from 'src/app/core/models/invoice-body.interface';
 import { InvoiceMaster } from 'src/app/core/models/invoice-master.interface';
 import { InvoiceTail } from 'src/app/core/models/invoice-tail.interface';
+import { Item } from 'src/app/core/models/item.interface';
 import { User } from 'src/app/core/models/user';
 import { HttpCommunicationsService } from 'src/app/core/services/http-communications.service';
 
@@ -12,8 +14,28 @@ import { HttpCommunicationsService } from 'src/app/core/services/http-communicat
 })
 export class InvoicesComponent implements OnInit {
   currentUser:User;
-  constructor(private httpService: HttpCommunicationsService) { }
   invoices:InvoiceMaster[];
+  invoiceForm:FormGroup;
+  itemForm:FormGroup;
+  rows:InvoiceBody[]=[];
+  invoiceToSave:InvoiceMaster;
+  validatingForm: FormGroup;
+  items: Item[];
+  constructor(private httpService: HttpCommunicationsService,private fb: FormBuilder) { 
+  this.invoiceForm = this.fb.group({
+    accountholder: ['', Validators.required],
+    date: ['', Validators.required],
+    paymentMethod: ['', Validators.required],
+    rows: ['', Validators.required],
+    tail: ['', Validators.required],
+  })
+  this.itemForm = this.fb.group({
+    number:[],
+    quantity:[""],
+    percDiscount:[0],
+  })
+}
+
   arrayOne(n: number): any[] {
     return Array(n);
   }
@@ -23,7 +45,21 @@ export class InvoicesComponent implements OnInit {
     this.httpService.retrieveGetCall<InvoiceMaster[]>("user/"+ this.currentUser.id +"/invoices").subscribe(response => {
       this.invoices = response;
     });
+    this.httpService.retrieveGetCall<Item[]>("item").subscribe(response => {
+      this.items = response;
+    })
   }
+
+  save(){
+    this.invoiceToSave.tail.discountPerc = this.invoiceForm.value.tail;
+    this.httpService.retrievePostCall<InvoiceMaster>("invoice/save",this.invoiceToSave)
+}
+ add(){
+  /*let newitem = this.items.find(it => it.id == 
+  let body:InvoiceBody = {finalAmount: 0,item:newitem,netPrice:0,percDiscount:this.itemForm.value.percDiscount,quantity:this.itemForm.value.quantity,taxable:0,taxed:0,totDiscount:0};
+  this.rows.push(body)*/
+  console.log(this.rows)
+}
 
 }
 
