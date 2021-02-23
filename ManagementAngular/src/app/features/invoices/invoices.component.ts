e764d4c16f3ff599410843c02b158020d287d912
AGhilardi <57990391+AGhilardi@users.noreply.github.com>
2021-02-23 10:58:10 +0100

    WIP invoices

--- ManagementAngular/src/app/features/invoices/invoices.component.ts
+++ ManagementAngular/src/app/features/invoices/invoices.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { InvoiceBody } from 'src/app/core/models/invoice-body.interface';
 import { InvoiceMaster } from 'src/app/core/models/invoice-master.interface';
 import { InvoiceTail } from 'src/app/core/models/invoice-tail.interface';
@@ -12,8 +13,25 @@ import { HttpCommunicationsService } from 'src/app/core/services/http-communicat
 })
 export class InvoicesComponent implements OnInit {
   currentUser:User;
-  constructor(private httpService: HttpCommunicationsService) { }
   invoices:InvoiceMaster[];
+  invoiceForm:FormGroup;
+  itemForm:FormGroup;
+  invoiceToSave:InvoiceMaster;
+  validatingForm: FormGroup;
+  constructor(private httpService: HttpCommunicationsService,private fb: FormBuilder) { 
+  this.invoiceForm = this.fb.group({
+    accountholder: ['', Validators.required],
+    date: ['', Validators.required],
+    paymentMethod: ['', Validators.required],
+    rows: ['', Validators.required],
+    tail: ['', Validators.required],
+  })
+  this.itemForm = this.fb.group({
+    quantity:[""],
+    percDiscount:[""],
+    name:[""],
+  })
+}
   arrayOne(n: number): any[] {
     return Array(n);
   }
@@ -25,5 +43,11 @@ export class InvoicesComponent implements OnInit {
     });
   }
 
+  save(){
+    this.invoiceToSave.tail.discountPerc = this.invoiceForm.value.tail;
+    this.httpService.retrievePostCall<InvoiceMaster>("invoice/save",this.invoiceToSave)
 }
+ add(){
 
+}
+}
