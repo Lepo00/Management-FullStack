5c3ace4ff2ff6a577c01ce9e7f50bc230f05d14c
Lepo00 <luca.leporati@outlook.com>
2021-02-24 15:00:11 +0100

    number deleted

--- BackEnd/ManagementSpring/src/main/java/it/jac/management/model/InvoiceMaster.java
+++ BackEnd/ManagementSpring/src/main/java/it/jac/management/model/InvoiceMaster.java
@@ -27,9 +27,6 @@ public class InvoiceMaster extends AuditModel {
 	@Temporal(TemporalType.DATE)
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
 	private Date date;
-
-	@Column(name = "number")
-	private int number;
 	
 	@Column(name = "payment_method")
 	private String paymentMethod;
@@ -56,14 +53,6 @@ public class InvoiceMaster extends AuditModel {
 
 	public void setDate(Date date) {
 		this.date = date;
-	}
-
-	public int getNumber() {
-		return number;
-	}
-
-	public void setNumber(int number) {
-		this.number = number;
 	}
 
 	public String getPaymentMethod() {
