095ca0710bb556092fc8517c1c36e17cd0485776
Lepo00 <luca.leporati@outlook.com>
2021-03-10 18:28:43 +0100

    Apache POI creation XSLX File

--- BackEnd/ManagementSpring/src/main/java/it/jac/management/controller/InvoiceController.java
+++ BackEnd/ManagementSpring/src/main/java/it/jac/management/controller/InvoiceController.java
@@ -1,7 +1,19 @@
 package it.jac.management.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -54,7 +66,7 @@ public class InvoiceController {
 	public ResponseEntity<?> newInvoice(@PathVariable Long idCustomer, @RequestBody InvoiceMaster invoice)
 			throws Exception {
 		try {
-			InvoiceMaster save=invoiceService.createWithCustomer(idCustomer, invoice);
+			InvoiceMaster save = invoiceService.createWithCustomer(idCustomer, invoice);
 			return ResponseEntity.ok(save);
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().body(new ResponseMessage("Invoice Not Saved!"));
@@ -80,5 +92,76 @@ public class InvoiceController {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseMessage("Invoice doesn't exists"));
 		}
 	}
-	
+
+	@GetMapping(path = "/{id}/csv")
+	public void exportCsv(@PathVariable Long id, HttpServletResponse response) throws IOException {
+		response.setContentType("application/vnd.ms-excel");
+		response.setHeader("Content-Disposition", "attachment; filename=Fattura" + id + ".xlsx");
+		InvoiceMaster invoice = invoiceService.get(id).get();
+		// Create a Workbook
+		Workbook workbook = new XSSFWorkbook(); // new HSSFWorkbook() for generating `.xls` file
+
+		/*
+		 * CreationHelper helps us create instances of various things like DataFormat,
+		 * Hyperlink, RichTextString etc, in a format (HSSF, XSSF) independent way
+		 */
+		CreationHelper createHelper = workbook.getCreationHelper();
+
+		// Create a Sheet
+		Sheet sheet = workbook.createSheet("Employee");
+
+		// Create a Font for styling header cells
+		Font headerFont = workbook.createFont();
+		headerFont.setBold(true);
+		headerFont.setFontHeightInPoints((short) 14);
+		headerFont.setColor(IndexedColors.RED.getIndex());
+
+		// Create a CellStyle with the font
+		CellStyle headerCellStyle = workbook.createCellStyle();
+		headerCellStyle.setFont(headerFont);
+
+		// Create a Row
+		Row headerRow = sheet.createRow(0);
+
+		String[] columns = { "Num.", "Intestatario", "Data", "Pagamento" };
+		// Create cells
+		for (int i = 0; i < columns.length; i++) {
+			Cell cell = headerRow.createCell(i);
+			cell.setCellValue(columns[i]);
+			cell.setCellStyle(headerCellStyle);
+		}
+
+		// Create Cell Style for formatting Date
+		CellStyle dateCellStyle = workbook.createCellStyle();
+		dateCellStyle.setDataFormat(createHelper.createDataFormat().getFormat("dd-MM-yyyy"));
+
+		// Create Other rows and cells with employees data
+		int rowNum = 1;
+		Row row = sheet.createRow(rowNum++);
+
+		row.createCell(0).setCellValue(invoice.getId());
+
+		row.createCell(1).setCellValue(invoice.getAccountholder());
+
+		Cell date = row.createCell(2);
+		date.setCellValue(invoice.getDate());
+		date.setCellStyle(dateCellStyle);
+
+		row.createCell(3).setCellValue(invoice.getPaymentMethod());
+
+		Row rows = sheet.createRow(3);
+		rows.createCell(0).setCellValue("prova");
+
+		// Resize all columns to fit the content size
+		for (int i = 0; i < columns.length; i++) {
+			sheet.autoSizeColumn(i);
+		}
+
+		// Write the output to a file
+		workbook.write(response.getOutputStream());
+
+		// Closing the workbook
+		workbook.close();
+	}
+
 }
